13080b810436dcecb8c88eb8d75a832eddd7fe4d
lukslevino <lukslevino@gmail.com>
2017-11-18 01:09:22 -0200

    [ADD] Conexao com o BD , Entidades mapeadas e estrututra MVC completa

--- fontes/module/Application/config/module.config.php
+++ fontes/module/Application/config/module.config.php
@@ -99,4 +99,24 @@ return array(
             ),
         ),
     ),
+    'doctrine' => array(
+        'driver' => array(
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'my_annotation_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . "/src/Application/Model"
+                ),
+            ),
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => array(
+                'drivers' => array(
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'Application\Model' => 'my_annotation_driver'
+                )
+            )
+        )
+    )
 );
---
+++ fontes/module/Application/src/Application/Controller/AcessoController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lazevedol
+ * Date: 18/11/2017
+ * Time: 01:00
+ */
+
+namespace Application\Controller;
+
+
+class AcessoController extends ApplicationAbstractController
+{
+
+}
